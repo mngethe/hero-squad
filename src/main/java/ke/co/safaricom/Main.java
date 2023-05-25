637e8489a8dfa3c4368117020406984d2448a7aa
mngethe <njeringethe@gmail.com>
2023-05-25 20:13:56 +0300

    commit all

---
+++ src/main/java/ke/co/safaricom/Main.java
@@ -0,0 +1,50 @@
+package ke.co.safaricom;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Handler;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", (req, res)->{
+            Map<String, String> payload = new HashMap<>();
+           return new ModelAndView(payload,"home.hbs.html");
+       }, new HandlebarsTemplateEngine());
+
+        get("/login", (req, res)->{
+            Map<String, String> payload = new HashMap<>();
+            return new ModelAndView(payload,"hero-form.hbs.html");
+        }, new HandlebarsTemplateEngine());
+
+        get("/login", (req, res)->{
+            Map<String, String> payload = new HashMap<>();
+            return new ModelAndView(payload,"squad-form.hbs.html");
+        }, new HandlebarsTemplateEngine());
+
+        post("/hero", (req, res)->{
+            String name = req.queryParams("name");
+            Integer age = Integer.parseInt(req.queryParams("age"));
+            String power = req.queryParams("power");
+            String weakness = req.queryParams("weakness");
+            Map<String, String> payload = new HashMap<>();
+            return new ModelAndView(payload,"success.hbs.html");
+        }, new HandlebarsTemplateEngine());
+
+        post("/squad", (req, res)->{
+            String name = req.queryParams("name");
+            Integer age = Integer.parseInt(req.queryParams("age"));
+            String power = req.queryParams("power");
+            String weakness = req.queryParams("weakness");
+            Map<String, String> payload = new HashMap<>();
+            return new ModelAndView(payload,"success.hbs.html");
+        }, new HandlebarsTemplateEngine());
+
+
+    }
+}
