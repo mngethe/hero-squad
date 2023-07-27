972d9329cc82437b5a41ab851af2ee0d3d0066c6
mngethe <njeringethe@gmail.com>
2023-07-27 21:40:09 +0300

    add README.md file changes sql

--- src/main/java/ke/co/safaricom/dao.java
+++ src/main/java/ke/co/safaricom/dao.java
@@ -1,4 +1,15 @@
 package ke.co.safaricom;
 
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class dao {
+    private static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/java two","postgres", "Ngethe1010");
+    public void  getALLHeroes(){
+        String query = "SELECT * FROM heroes;";
+
+       try(Connection connection = Sql2o.open()){
+
+       }
+       }catch ()
 }
